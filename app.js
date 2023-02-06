5ad0d09d98baffce7f4b6f49bfdf91f12a05b78d
Eric Rouviere <eric.rouviere2@sncf.fr>
2023-02-06 11:30:51 +0100

    gyefu

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const port = process.env.APP_PORT ?? 5002;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -16,17 +16,17 @@ const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.putMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
-const usersHandlers = require("./usersHandlers");
+const userHandlers = require("./userHandlers");
+const { hashPassword } = require("./auth.js");
 
-app.get("/api/users", usersHandlers.getUsers);
-app.get("/api/users/:id", usersHandlers.getUsersById);
-app.post("/api/users", usersHandlers.postUsers);
-app.put("/api/users/:id", usersHandlers.putUsers);
-app.delete("/api/users/:id", usersHandlers.deleteUsers);
-
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", hashPassword, userHandlers.postUser);
+app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
@@ -35,3 +35,6 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+
+
--- movieHandlers.js
+++ movieHandlers.js
@@ -1,34 +1,33 @@
-const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    colors: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    colors: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
 const database = require("./database");
 
 const getMovies = (req, res) => {
+  const initialSql = "select * from movies";
+  const where = [];
+
+  if (req.query.color != null) {
+    where.push({
+      column: "color",
+      value: req.query.color,
+      operator: "=",
+    });
+  }
+  if (req.query.max_duration != null) {
+    where.push({
+      column: "duration",
+      value: req.query.max_duration,
+      operator: "<=",
+    });
+  }
+
   database
-    .query("select * from movies")
+    .query(
+      where.reduce(
+        (sql, { column, operator }, index) =>
+          `${sql} ${index === 0 ? "where" : "and"} ${column} ${operator} ?`,
+        initialSql
+      ),
+      where.map(({ value }) => value)
+    )
     .then(([movies]) => {
       res.json(movies);
     })
@@ -37,6 +36,7 @@ const getMovies = (req, res) => {
       res.status(500).send("Error retrieving data from database");
     });
 };
+
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
@@ -71,7 +71,8 @@ const postMovie = (req, res) => {
       res.status(500).send("Error saving the movie");
     });
 };
-const putMovie = (req, res) => {
+
+const updateMovie = (req, res) => {
   const id = parseInt(req.params.id);
   const { title, director, year, color, duration } = req.body;
 
@@ -92,8 +93,10 @@ const putMovie = (req, res) => {
       res.status(500).send("Error editing the movie");
     });
 };
+
 const deleteMovie = (req, res) => {
   const id = parseInt(req.params.id);
+
   database
     .query("delete from movies where id = ?", [id])
     .then(([result]) => {
@@ -113,7 +116,6 @@ module.exports = {
   getMovies,
   getMovieById,
   postMovie,
-  putMovie,
-  deleteMovie // don't forget to export your function ;)
+  updateMovie,
+  deleteMovie,
 };
-
